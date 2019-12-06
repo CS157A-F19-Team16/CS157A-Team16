85d1f2ee60f66a2c2642ae377046639523e38ed9
Jeffrey Zhang <zjeffreyl@gmail.com>
2019-12-06 14:06:58 -0800

    Early comments prototyping.

--- outdoorclimb/frontend/src/components/routes/RoutesViewer.js
+++ outdoorclimb/frontend/src/components/routes/RoutesViewer.js
@@ -123,6 +123,42 @@ export class RoutesViewer extends Component {
                 </div>
               </div>
             </div>
+            <div>
+              <div className="card card-body mt-5">
+                <div className="row">
+                  <div class="media">
+                    <div class="media-body">
+                      <h5 class="mt-0">Media heading</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel
+                      metus scelerisque ante sollicitudin. Cras purus odio,
+                      vestibulum in vulputate at, tempus viverra turpis. Fusce
+                      condimentum nunc ac nisi vulputate fringilla. Donec
+                      lacinia congue felis in faucibus.
+                    </div>
+                  </div>
+                  <div class="media">
+                    <div class="media-body">
+                      <h5 class="mt-0">Media heading</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel
+                      metus scelerisque ante sollicitudin. Cras purus odio,
+                      vestibulum in vulputate at, tempus viverra turpis. Fusce
+                      condimentum nunc ac nisi vulputate fringilla. Donec
+                      lacinia congue felis in faucibus.
+                    </div>
+                  </div>
+                  <div class="media">
+                    <div class="media-body">
+                      <h5 class="mt-0">Media heading</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel
+                      metus scelerisque ante sollicitudin. Cras purus odio,
+                      vestibulum in vulputate at, tempus viverra turpis. Fusce
+                      condimentum nunc ac nisi vulputate fringilla. Donec
+                      lacinia congue felis in faucibus.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </form>
       </div>
